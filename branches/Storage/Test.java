b8af7f78f64f4c8d983dc052895ef56bfb10e775
ibaun <ibaun@c8f621bb-e712-0410-9e54-ea9c5dbcbee5>
2006-05-17 12:14:57 +0000

    Tests by Birger.

--- branches/Storage/Test.java
+++ branches/Storage/Test.java
@@ -1,59 +1,25 @@
 package Storage;
-import java.util.ArrayList;
+
 import java.util.Iterator;
 import java.util.List;
 
-import Exercise.*;
+import Exercise.Exercise;
 
 public class Test {
 	
 	private static DataConnection conn = new DataConnection();
 	
-	public static void main(String[] args){        
+	public static void main(String[] args){
 		conn.connect();
 		
-		TestPrint();
+		List<String> list = conn.getThemes();
 		
 		conn.close();
-	}
-	
-	public static void TestAdd(){
-		for(int i=0;i<2500;i++){
-			List<String> pos = new ArrayList<String>();
-			pos.add("A");
-			pos.add("B");
-			pos.add("C");
-			pos.add("D");
-			Exercise ex = new MultichoiceExercise("exercise "+i,"main","What is this question",pos,1);
-			conn.addExercise(ex);
-		}
-		for(int i=2500;i<5000;i++){
-			List<String> pos = new ArrayList<String>();
-			pos.add("A");
-			pos.add("B");
-			pos.add("C");
-			pos.add("D");
-			Exercise ex = new MultichoiceExercise("exercise "+i,"sub","What is this question",pos,2);
-			conn.addExercise(ex);
-		}
-	}
-	
-	public static void TestGet(){
-		System.out.println(conn.getExercise("exercise 3"));
-	}
-	
-	public static void TestRemove(){
-		for(int i=0;i<5000;i++){
-			conn.deleteExercise("exercise "+i);
-		}
-	}
-	
-	public static void TestPrint(){
-		List<String> list = conn.getExerciseTypes();
+		
 		Iterator iter = list.iterator();
-	    while(iter.hasNext())
-	    	System.out.println((String)(iter.next()));
-	    System.out.println(""+list.size());
+		while(iter.hasNext())
+			System.out.println((String)iter.next());
+				
 	}
 
 }
--- branches/Storage/TestStorage.java
+++ branches/Storage/TestStorage.java
@@ -1,10 +1,174 @@
 package Storage;
 
-import junit.framework.TestCase;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
-public class TestStorage extends TestCase {
+import javax.jdo.PersistenceManager;
 
-	public static void main(String[] args) {
+import junit.framework.Assert;
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
+
+import org.junit.*;
+
+import Exercise.Exercise;
+import Exercise.MultichoiceExercise;
+
+public class TestStorage {
+	
+	private DataConnection conn = new DataConnection();
+	private Exercise[] buffer = new Exercise[500];
+	private Set<String> themes = new HashSet<String>();
+	
+	public static Test suite(){
+		return new JUnit4TestAdapter(TestStorage.class);
 	}
-
+	
+	@Before
+	public void testAdd(){
+		conn.connect();
+		
+		for(int i=0;i<250;i++){
+			List<String> pos = new ArrayList<String>();
+			pos.add("A");
+			pos.add("B");
+			pos.add("C");
+			pos.add("D");
+			String theme = "algemeen";
+			Exercise ex = new MultichoiceExercise("exercise "+i,theme,"What is this question",pos,1);
+			buffer[i] = ex;
+			themes.add(theme);
+			conn.addExercise(ex);
+		}
+		for(int i=250;i<500;i++){
+			List<String> pos = new ArrayList<String>();
+			pos.add("A");
+			pos.add("B");
+			pos.add("C");
+			pos.add("D");
+			String theme = "expert";
+			Exercise ex = new MultichoiceExercise("exercise "+i,theme,"What is this question",pos,2);
+			buffer[i] = ex;
+			themes.add(theme);
+			conn.addExercise(ex);
+		}
+		
+		conn.close();
+	}
+	
+	//FIXME: equals methods of exercise types
+	@org.junit.Test
+	public void testGet(){
+		conn.connect();
+		for (int i=0; i<500; i++)
+			Assert.assertEquals(buffer[i].toString(), (conn.getExercise("exercise "+ i)).toString());
+		conn.close();
+	}
+	
+	@org.junit.Test
+	public void testGetThemes(){
+		conn.connect();
+		ArrayList<String> dbthemes = (ArrayList<String>)conn.getThemes();
+		for (int i=0; i<dbthemes.size(); i++)
+			Assert.assertEquals((setToList(themes)).size(), dbthemes.size());
+		conn.close();
+	}
+	
+	@org.junit.Test
+	public void testGetTheme(){
+		conn.connect();
+		List<String> themes = conn.getThemes();
+		for (int i=0;i<themes.size();i++){
+			List<String> tmp = new ArrayList<String>();
+			String theme = themes.get(i);
+	    	for (int j=0; j<buffer.length; j++){
+	    		if (buffer[j].getTheme().equals(theme))
+	    		tmp.add((String)buffer[j].getTheme());
+	    	}
+			Assert.assertEquals(tmp, conn.getTheme(theme));
+		}
+		conn.close();		
+	}
+	
+	//Manually add the types to check!!
+	@org.junit.Test
+	public void testGetExerciseTypes(){
+		conn.connect();
+		Assert.assertEquals("MultichoiceExercise", (conn.getExerciseTypes()).get(0));
+		conn.close();
+	}
+	
+	
+	
+	@org.junit.Test
+	public void testGetExerciseType(){
+		Set<Exercise> tmp = new HashSet<Exercise>();
+		MultichoiceExercise dum = new MultichoiceExercise();
+		for(int i=0; i<buffer.length; i++){
+			if((dum.getClass()).equals(buffer[i].getClass()))
+				tmp.add(buffer[i]);
+		}
+			
+		conn.connect();
+		Assert.assertEquals(tmp, listToSet(conn.getExerciseType(dum)));
+		conn.close();
+	}
+	
+	@org.junit.Test
+	public void testGetExerciseNames(){
+		conn.connect();
+		
+		Set<String> tmp = new HashSet<String>();
+    	for (int i=0; i<buffer.length; i++)
+    		tmp.add(buffer[i].getTitle());
+    	
+		Assert.assertEquals(tmp, listToSet(conn.getExerciseNames()));
+		conn.close();
+	}
+	
+	//FIXME: equals methods of exercise types
+	@org.junit.Test
+	public void testGetAllExercises(){
+		conn.connect();
+		
+		Set<Exercise> tmp = new HashSet<Exercise>();
+    	for (int i=0; i<buffer.length; i++)
+    		tmp.add(buffer[i]);
+    	
+		for (int i=0; i<5000; i++)
+			Assert.assertEquals(tmp, listToSet(conn.getAllExercises()));
+		conn.close();
+	}
+	/*
+	@After
+	public void testRemove(){
+		conn.connect();
+		for(int i=0;i<5000;i++)
+			conn.deleteExercise("exercise "+i);
+		conn.close();
+	}
+	*/
+	/**
+     * Converts a set to a list
+     * @param set
+     * @return
+     */
+    private List<String> setToList(Set<String> set) {
+    	List<String> out = new ArrayList<String>();
+    	Iterator iter = set.iterator();
+    	while (iter.hasNext())
+    		out.add((String) iter.next());
+    	return out;    		
+    }
+    
+    private Set listToSet(List list) {
+    	Set out = new HashSet();
+    	Iterator iter = list.iterator();
+    	while (iter.hasNext())
+    		out.add(iter.next());
+    	return out;
+    }
 }
